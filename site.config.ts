83df32030e6b41ce85edc1f0a6b2bb147af60e69
Starry Wang <117701936+starrywang008@users.noreply.github.com>
2024-12-25 16:16:38 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -26,8 +26,8 @@ export default siteConfig({
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: '/icon.png',
-  defaultPageCover: '/images/cover.jpg',
+  defaultPageIcon: null,
+  defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
