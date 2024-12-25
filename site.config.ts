fe52394af566ea557a000a9c2af2636ae4181aee
Starry Wang <117701936+starrywang008@users.noreply.github.com>
2024-12-25 17:30:26 -0500

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,8 +17,6 @@ export default siteConfig({
   description: 'Starry Wang - Instructional Designer, Content Creator, and EdTech Advocate',
 
 // 修改为实际链接
-instagram: 'https://twitter.com/your_twitter_handle',
-github: 'https://github.com/your_github_username',
 linkedin: 'qingru-wang-5031811b4/',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
